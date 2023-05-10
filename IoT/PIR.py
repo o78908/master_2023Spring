294fe106929db65e30c548724a6bee0d88162d86
o78908 <o78908@gmail.com>
2023-05-10 21:19:52 +0800

    PIR 新增沒複製到的code

--- IoT/PIR.py
+++ IoT/PIR.py
@@ -24,3 +24,31 @@ Setup(3, "IN") #yellow
 Setup(3, "OUT")
 Setup(4, "IN") #green
 Setup(4, "OUT")
+
+
+
+count = 0
+ledChoice = [2,3,4]
+def motion(GPIOnum):
+    global count    ## 設定全域變數
+    
+    if GPIO.input(GPIOnum):
+        count+=1
+        print("motion detected {0}".format(count)) ## 印出偵測到的次數
+        led = random.choice(ledChoice)    ## 隨機選擇一個LED
+        print(f"turn on led {led}")     ## 印出選擇的LED
+        TurnOnLED(led)      ## 點亮LED
+        time.sleep(3)
+        TurnOffLED(led)     ## 熄滅LED
+
+    else:
+        print("no motion")
+
+try:
+    setup(14)
+    GPIO.add_event_detect(14, GPIO.BOTH, callback=motion, bouncetime=200 )
+    while True:  
+        time.sleep(1)
+except:
+    GPIO.cleanup()
+
