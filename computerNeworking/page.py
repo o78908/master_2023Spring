850cf7f0618867af1a86e16ed31c0499f30c34df
o78908 <o78908@gmail.com>
2023-06-11 21:51:47 +0800

    修改主程式的檔案路徑(履歷)

--- computerNeworking/page.py
+++ computerNeworking/page.py
@@ -8,7 +8,7 @@ import numpy as np
 import json
 import os
 ## You must add it in case file cannot be read by page.py!!!!
-os.chdir("C:/Users/Modern 14/Desktop/CNfinalproject-20230522T012228Z-001/CNfinalproject") ## 改成自己的路徑
+os.chdir("C:/Users/Modern 14/Desktop/gitRepo/master_2023Spring/computerNeworking") ## 改成自己的路徑
 ## You must add it in case file cannot be read by page.py!!!!
 app = Flask(__name__)
 @app.route('/')
